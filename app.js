a0021710788a2296e528e557c4e74d455d170f70
hugodieb.hd@gmail.com <hugodieb.hd@gmail.com>
2019-08-15 13:25:42 -0300

    usando tabela do bootstrap

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ let id, name, age,
     profession, active = [],
     mainO = {};
 
+let appContainer = document.getElementById("app");
+let apptable = document.getElementById("tb");
 
 function init() {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let myObj = JSON.parse(JSON.parse(this.responseText));
-        console.log(myObj)
-        document.getElementyById("app").innerHTML = `
-        ${obj.map(data => {
-            return `
-            <div class="alert alert-primary" role="alert">
-                ${data.NOME}
-            </div>
-            `
-        })}
-        `
+        display(myObj);
       }
     };
     xmlhttp.open("GET", "teste.json", true);
     xmlhttp.send();
 }
 
-function display(obj) {
+function display(data) {
+    let htmlString = "";
+    let names = []
+    data.forEach(resp => {
+        return names.push(resp.NOME)
+    })
+    console.log(names.length);
+    for (var i = 0; i < names.length; i++) {
+        // htmlString+= "<div class='alert alert-success' role='alert'>" + names[i] + "</div>"
+        htmlString+= "<tr> <th scope='row'>" + i + "</th> <td>" + names[i] + "</td> </tr>"
+        console.log(htmlString);
+    }
+    apptable.insertAdjacentHTML('beforeend', htmlString);
 
 }
 
